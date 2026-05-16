73adadceb0c6207f1d131e7cbe38146115280f57
an7708 <sleepanddrugs@gmail.com>
2026-05-16 16:34:35 +0530

    feat: prepare for production deployment

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,24 +1,14 @@
     import axios from 'axios';
-    import useAuthStore from '../store/useAuthStore';
 
     const api = axios.create({
     baseURL: process.env.REACT_APP_API_URL || 'http://localhost:5000/api',
     });
 
-    // api.interceptors.request.use((config) => {
-    // const token = localStorage.getItem('vitalsync_token');
-    // if (token) {
-    //     config.headers.Authorization = `Bearer ${token}`;
-    // }
-    // return config;
-    // });
     api.interceptors.request.use((config) => {
-    const token = useAuthStore.getState().token;
-
+    const token = localStorage.getItem('vitalsync_token');
     if (token) {
         config.headers.Authorization = `Bearer ${token}`;
     }
-
     return config;
     });
 
